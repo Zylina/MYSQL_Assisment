09a2d1b296992dfabc524f317f1e861a29a1a77e
Himanshi Gupta <93181334+Zylina@users.noreply.github.com>
2022-02-20 22:19:18 +0530

    Update Assestment-2.sql

--- Assestment-2.sql
+++ Assestment-2.sql
@@ -290,6 +290,11 @@ A002	ANDRD4	B008	F005	2010-06-05	2010-06-28
 A005	ANDRD4	B008	F005	2010-06-05	2010-06-28
 A006	ANDRD4	B009	F005	2011-08-01	2011-08-20
 
+/*Begin transaction & insert below to records into Login_Details
+'U001' Admin1@123
+'U002' Admin2@123
+Perform rollback operation & verify whether any records are inserted in table or not. */
+
 set autocommit = 0;
 start transaction; 
 insert into `Login_Details`(`User_Id` , `User_Password`) values('U001' ,'Admin1@123'),('U002', 'Admin2@123');
@@ -301,22 +306,25 @@ U002	Admin2@123
 
 ROLLBACK;
 
+/*Remove table Login_Details from database.*/
 DROP TABLE LOGIN_DETAILS;
 
-CREATE USER 'new_user'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON * . * TO 'new_user'@'localhost';
+/*Create a dummy user in database. Grant create & select table privilege to him/her.
+Repeat the above all queries using login credentials of newly created user.
+Revoke the privilege assigned to this newly created user*/
+
+create user student identified by "root";
+GRANT ALL PRIVILEGES ON * . * TO 'student';
 FLUSH PRIVILEGES;
-GRANT CREATE, SELECT ON * . * TO 'user_name'@'localhost';
-REVOKE ALL PRIVILEGES ON * . * FROM 'user_name'@'localhost';
-DROP USER ‘user_name’@‘localhost’;
-SHOW GRANTS FOR 'user_name'@'localhost';
+GRANT CREATE, SELECT ON * . * TO 'student';
+revoke create , select on * . * from student;
+SHOW GRANTS FOR 'student';
 
 OUTPUT:
- Grants for user_name@localhost                                             
+ # Grants for student@%
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, SHUTDOWN, PROCESS, FILE, REFERENCES, INDEX, ALTER, SHOW DATABASES, SUPER, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER, CREATE TABLESPACE, CREATE ROLE, DROP ROLE ON *.* TO `student`@`%`
+GRANT APPLICATION_PASSWORD_ADMIN,AUDIT_ABORT_EXEMPT,AUDIT_ADMIN,AUTHENTICATION_POLICY_ADMIN,BACKUP_ADMIN,BINLOG_ADMIN,BINLOG_ENCRYPTION_ADMIN,CLONE_ADMIN,CONNECTION_ADMIN,ENCRYPTION_KEY_ADMIN,FLUSH_OPTIMIZER_COSTS,FLUSH_STATUS,FLUSH_TABLES,FLUSH_USER_RESOURCES,GROUP_REPLICATION_ADMIN,GROUP_REPLICATION_STREAM,INNODB_REDO_LOG_ARCHIVE,INNODB_REDO_LOG_ENABLE,PASSWORDLESS_USER_ADMIN,PERSIST_RO_VARIABLES_ADMIN,REPLICATION_APPLIER,REPLICATION_SLAVE_ADMIN,RESOURCE_GROUP_ADMIN,RESOURCE_GROUP_USER,ROLE_ADMIN,SERVICE_CONNECTION_ADMIN,SESSION_VARIABLES_ADMIN,SET_USER_ID,SHOW_ROUTINE,SYSTEM_USER,SYSTEM_VARIABLES_ADMIN,TABLE_ENCRYPTION_ADMIN,XA_RECOVER_ADMIN ON *.* TO `student`@`%`
 
- GRANT USAGE ON *.* TO 'user_name'@'localhost'                              
- GRANT ALL PRIVILEGES ON 'database_name'.* TO 'user_name'@'localhost'.      
- REVOKE ALL PRIVILEGES ON * . * FROM 'user_name'@'localhost'; 
 
 
 
